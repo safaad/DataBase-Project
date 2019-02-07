5c88af0b5f063275bee6f3dc65be72c30b58246d
bakernaim <baker_naim_1999@hotmail.com>
2019-02-07 20:43:51 +0200

    test

--- src/Driver/Main.java
+++ src/Driver/Main.java
@@ -1,23 +1,11 @@
 package Driver;
 
-import Model.DataBaseConnection;
+
 import javafx.application.Application;
-import javafx.beans.property.SimpleStringProperty;
-import javafx.beans.value.ObservableValue;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
-import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
 import javafx.stage.Stage;
-import javafx.util.Callback;
-
-import java.sql.Connection;
-import java.sql.ResultSet;
 
 public class Main extends Application {
     @Override
@@ -32,4 +20,5 @@ public class Main extends Application {
     public static void main(String[] args) {
         launch(args);
     }
+
 }
